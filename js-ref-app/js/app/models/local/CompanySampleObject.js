9c2487ab951d406960c164c23421b93e1bda840e
cristian-spiescu <cristian.spiescu@crispico.com>
2013-10-30 22:53:58 +0100

    created CompanySampleObjet

---
+++ js-ref-app/js/app/models/local/CompanySampleObject.js
@@ -0,0 +1,18 @@
+// template backboneClass
+define(function (require) {
+    
+    "use strict";
+    
+    var $ = require('jquery');
+    var Class = require('app/models/local/abstract/Class');
+    var _ = require('underscore');// children-insert-point requireEntry
+    
+    return Class.extend({
+    
+    	id : "", 
+    	name : "Company name", 
+    	industry : "Industry"// children-insert-point backboneClassMember
+    
+    })
+
+});
